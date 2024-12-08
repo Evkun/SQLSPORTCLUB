7f85da46a01b5e9f226faba6cb45ad01e16ed3a8
Evkun <113624128+Evkun@users.noreply.github.com>
2024-12-08 19:54:12 +0300

    Create ТаблицаТренеров.sql

---
+++ ТаблицаТренеров.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `gymsport`.`тренеры` (
+  `Id_trene` INT NOT NULL,
+  `Фамилия` VARCHAR(45) NOT NULL,
+  `Имя` VARCHAR(45) NOT NULL,
+  `Отчество` VARCHAR(45) NOT NULL,
+  `цена занятия` INT NOT NULL,
+  PRIMARY KEY (`Id_trene`));
