06396863b7897274f18dfa73e96d061d13db96cf
Evkun <113624128+Evkun@users.noreply.github.com>
2024-12-08 21:04:04 +0300

    Update ТаблицаТренеров.sql

--- ТаблицаТренеров.sql
+++ ТаблицаТренеров.sql
@@ -5,3 +5,7 @@ CREATE TABLE `gymsport`.`тренеры` (
   `Отчество` VARCHAR(45) NOT NULL,
   `цена занятия` INT NOT NULL,
   PRIMARY KEY (`Id_trene`));
+INSERT INTO `gymsport`.`тренеры` (`Id_trene`, `Фамилия`, `Имя`, `Отчество`, `цена занятия`) VALUES ('1', 'Ivanov', 'Ivan', 'Ivanovich', '1500');
+INSERT INTO `gymsport`.`тренеры` (`Id_trene`, `Фамилия`, `Имя`, `Отчество`, `цена занятия`) VALUES ('2', 'Dzuba', 'Artem', 'Olegovich', '1000');
+INSERT INTO `gymsport`.`тренеры` (`Id_trene`, `Фамилия`, `Имя`, `Отчество`, `цена занятия`) VALUES ('3', 'Novikov', 'Ivan', 'Matveevich', '1000');
+INSERT INTO `gymsport`.`тренеры` (`Id_trene`, `Фамилия`, `Имя`, `Отчество`, `цена занятия`) VALUES ('4', 'Suvorov', 'Evgeniy', 'Pavlovich', '3000');
